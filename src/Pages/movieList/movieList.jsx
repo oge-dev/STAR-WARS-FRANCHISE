533f2705a48b80a4bcddaf2b12ded90aa5e19294
julietoge <julietogechi27@gmail.com>
2024-06-21 07:45:37 +0100

    updated

--- src/Pages/movieList/movieList.jsx
+++ src/Pages/movieList/movieList.jsx
@@ -2,7 +2,18 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import Loader from "../../components/loader/loader.jsx";
-import "./movieList.css"
+import "./movieList.css";
+
+// Import background images
+import bgCard1 from "../../assets/bg-card-1.png";
+import bgCard2 from "../../assets/bg-card-2.png";
+import bgCard3 from "../../assets/bg-card-3.png";
+import bgCard4 from "../../assets/bg-card-4.png";
+import bgCard5 from "../../assets/bg-card-5.png";
+import bgCard6 from "../../assets/bg-card-6.png";
+
+// Create an array of background images
+const backgrounds = [bgCard1, bgCard2, bgCard3, bgCard4, bgCard5, bgCard6];
 
 const MovieList = () => {
   const [movies, setMovies] = useState([]);
@@ -13,8 +24,7 @@ const MovieList = () => {
     axios
       .get("https://swapi.dev/api/films/")
       .then((response) => {
-        setMovies(response.data.results);
-        console.log(response.data);
+        setMovies(response.data.results.slice(0, 6)); // Limiting to 6 movies
         setError(null);
       })
       .catch((error) => {
@@ -36,20 +46,24 @@ const MovieList = () => {
         )}
       </div>
       <div className="movieItems">
-        {movies.map((movie, id) => (
-          <div className="movie" key={movie.episode_id}>
-            <h3 className="title">{movie.title}</h3>
-            <span className="release_date">
-              {new Date(movie.release_date).toLocaleDateString("en-US", {
-                month: "long",
-                day: "numeric",
-                year: "numeric",
-              })}
-            </span>
-            <p className="opening_crawl">
-              {movie.opening_crawl}
-            </p>
-            <Link to={`/movie/${id + 1}`}>More info</Link>
+        {movies.map((movie, index) => (
+          <div
+            className="movie"
+            key={movie.episode_id}
+            style={{ backgroundImage: `url(${backgrounds[index]})` }}
+          >
+            <div className="movie-content">
+              <h3 className="title">{movie.title}</h3>
+              <span className="release_date">
+                {new Date(movie.release_date).toLocaleDateString("en-US", {
+                  month: "long",
+                  day: "numeric",
+                  year: "numeric",
+                })}
+              </span>
+              <p className="opening_crawl">{movie.opening_crawl}</p>
+              <Link to={`/movie/${index + 1}`}>More info</Link>
+            </div>
           </div>
         ))}
       </div>
