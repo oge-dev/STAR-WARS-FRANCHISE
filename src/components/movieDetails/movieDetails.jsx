3cb7264b7ba003fdf50ee4c4312fc6cbe4be0df2
julietoge <julietogechi27@gmail.com>
2024-06-21 17:05:28 +0100

    updated

--- src/components/movieDetails/movieDetails.jsx
+++ src/components/movieDetails/movieDetails.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import "./movieDetails.css"
+import "./movieDetails.css";
 
 const MovieDetails = ({ movie }) => {
   const [details, setDetails] = useState({
@@ -30,44 +30,63 @@ const MovieDetails = ({ movie }) => {
   return (
     <div className="movieDetails-container">
       <div className="movieDetails">
-      <Link to="/">← Back to list</Link>
-      <h2>{movie.title}</h2>
-      <p>Director: {movie.director}</p>
-      <p>Producer: {movie.producer}</p>
-      <p>Description: {movie.opening_crawl}</p>
-      <h3>Characters</h3>
-      <ul>
-        {details.characters.map((character) => (
-          <li key={character.url}>{character.name}</li>
-        ))}
-      </ul>
-      <h3>Planets</h3>
-      <ul>
-        {details.planets.map((planet) => (
-          <li key={planet.url}>{planet.name}</li>
-        ))}
-      </ul>
-      <h3>Species</h3>
-      <ul>
-        {details.species.map((species) => (
-          <li key={species.url}>{species.name}</li>
-        ))}
-      </ul>
-      <h3>Starships</h3>
-      <ul>
-        {details.starships.map((starship) => (
-          <li key={starship.url}>{starship.name}</li>
-        ))}
-      </ul>
+        <Link to="/">← Back to list</Link>
+        <div className="title-director-producer">
+          <h2>{movie.title}</h2>
+          <p>Director: {movie.director}</p>
+          <p>Producer: {movie.producer}</p>
+        </div>
+        <div className="desc">
+          <h4>Description</h4>
+          <p>{movie.opening_crawl}</p>
+        </div>
+        <hr className="hr" />
+        <div className="characters">
+          <h3>Characters</h3>
+          <ul>
+            {details.characters.map((character) => (
+              <li key={character.url}>{character.name}</li>
+            ))}
+          </ul>
+        </div>
+        <hr className="hr" />
+        <div className="planets">
+          <h3>Planets</h3>
+          <ul>
+            {details.planets.map((planet) => (
+              <li key={planet.url}>{planet.name}</li>
+            ))}
+          </ul>
+        </div>
+        <hr className="hr" />
+        <div className="species">
+          <h3>Species</h3>
+          <ul>
+            {details.species.map((species) => (
+              <li key={species.url}>{species.name}</li>
+            ))}
+          </ul>
+        </div>
+        <hr className="hr" />
+        <div className="starships">
+          <h3>Starships</h3>
+          <ul>
+            {details.starships.map((starship) => (
+              <li key={starship.url}>{starship.name}</li>
+            ))}
+          </ul>
+        </div>
 
-      <h3>Vehicles</h3>
-      <ul>
-        {details.vehicles.map((vehicle) => (
-          <li key={vehicle.url}>{vehicle.name}</li>
-        ))}
-      </ul>
+        <hr className="hr" />
+        <div className="vehicle">
+          <h3>Vehicles</h3>
+          <ul>
+            {details.vehicles.map((vehicle) => (
+              <li key={vehicle.url}>{vehicle.name}</li>
+            ))}
+          </ul>
+        </div>
       </div>
-      
     </div>
   );
 };
